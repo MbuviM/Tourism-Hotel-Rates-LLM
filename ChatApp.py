37c9e09995df8d1f7da1d5316aacaf187b58a14a
MbuviM <mwendembuvi10@gmail.com>
2024-08-13 06:47:34 +0300

    Streamlit Deployment

--- ChatApp.py
+++ ChatApp.py
@@ -5,10 +5,10 @@ import os
 import re
 
 # Access API key from Streamlit secrets
-api_key = st.secrets["OPENAI_API_KEY"]
-assistant_id = st.secrets["ASSISTANT_ID"]
-vector_store_id = st.secrets["VECTOR_STORE_ID"]
-file_id = st.secrets["FILE_ID"]
+api_key = st.secrets["open-ai"]["OPENAI_API_KEY"]
+assistant_id = st.secrets["assistants"]["ASSISTANT_ID"]
+vector_store_id = st.secrets["assistants"]["VECTOR_STORE_ID"]
+file_id = st.secrets["assistants"]["FILE_ID"]
 
 # Load environment variables and assistant details
 load_dotenv()
